4162e49d4e61f69e011ad5254217e2e29f6158f1
chagall831117 <chagall831117@users.noreply.github.com>
2020-01-07 16:07:19 +0800

    完成

---
+++ UnityPetFollow/Assets/Scripts/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    public Transform Dragon;
+    public Transform Spot;
+    public Transform Man;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dragon = GetComponent<Transform>();
+        Spot = GameObject.Find("Quad").GetComponent<Transform>();
+        Man = GameObject.Find("野蠻人").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tracking();
+        transform.LookAt(Man);
+    }
+    public void Tracking()
+    {
+        Dragon.position = Vector3.Lerp(Dragon.position, Spot.position,
+            1f*Time.deltaTime);
+    }
+}
